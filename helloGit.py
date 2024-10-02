aa8a3cd799b5dd5298cae45841c2be2dd4394586
Sarwegt <avilaisaias0607@gmail.com>
2024-10-02 13:16:52 -0600

    Se actualiza el commit

--- helloGit.py
+++ helloGit.py
@@ -1,2 +1,16 @@
-print ("Hello Git")
-print ("Que pedorrito")
+import random
+num=[]
+aux=0
+for i in range (10):
+    aleatorios=random.randrange(1,30)
+    num.append(aleatorios)
+print (num)
+
+for i in range (9):
+    for j in range(9):
+        if num[j] > num[j+1]:
+            aux=num[j]
+            num[j]= num[j+1]
+            num[j+1]= aux
+
+print (num)
